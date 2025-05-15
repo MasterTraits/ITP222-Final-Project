27a5f49f56a19194b5e21eaf5fc4288fc9bad04d
Emmanuel Fabella <efrohibire@gmail.com>
2025-05-16 05:49:18 +0800

    style: index.css for u guys to reuse

--- app/views/home.php
+++ app/views/home.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Compass</title>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+    body { font-family: 'Inter', sans-serif; }
+    :root {
+      --text-dark: #444;
+      --gold: #FC6;
+      --blue: #069;
+      --background: #F4EEEC;
+      --bg-transparent-dark: rgba(0, 0, 0, 0.50);
+    }
+  </style>
 </head>
+
 <body>
-  This is the landing page of the Compass application
-  <a href="logout"><button>Log-out?</button></a> 
+  <nav class="fixed top-5 left-1/2 transform -translate-x-1/2 z-10 bg-">
+    <a href="/" class="flex items-center">
+      <img src="assets/logo.svg" alt="Compass Logo" class="h-10 w-auto">
+      <span class="text-2xl font-bold text-[#333333] ml-2">Compass</span>
+    </a>
+
+  </nav>
+  <section class="h-150 bg-black text-white">
+    Hello
+  </section>
+  <section>
+
+  </section>
 </body>
+
 </html>
